30201e73f9f2f9ede552c2f16a67d07175df32d7
Subhashini M <ssubashinim@gmail.com>
2025-07-23 17:22:55 +0530

    figma_recreation

---
+++ android/app/src/main/kotlin/com/example/figma_recreataion/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.figma_recreataion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        leading: Icon(Icons.menu, color: Colors.white),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.phone),
+              label: Text("Help"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+                shape: StadiumBorder(side: BorderSide(color: Colors.white)),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(30),
+                ),
+              ),
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 28,
+                        backgroundColor: Colors.white,
+                        child: Icon(Icons.person, color: Colors.grey, size: 50),
+                      ),
+                      Text(
+                        "Hello",
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                      Container(
+                        color: Colors.yellow,
+                        child: Text(
+                          "UNVERIFIED",
+                          style: TextStyle(color: Colors.black, fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "Raguvaran Kanth Kumareshan ",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  SizedBox(
+                    width: 400.0,
+                    height: 200.0,
+                    child: Card(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Verify Your KYC',
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            "lorem ipsum is a simple dumm",
+                            style: TextStyle(fontSize: 17),
+                          ),
+                          SizedBox(height: 40),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(8),
+                                color: Colors.blue,
+                                child: Text(
+                                  "Verify Now",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "My Trucks",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      CircleAvatar(
+                        child: Icon(Icons.add, color: Colors.white),
+                        backgroundColor: Colors.blue,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "lorem ipsum is a simple dumm",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  SizedBox(height: 100),
+                  Center(
+                    child: Text(
+                      "no data",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
